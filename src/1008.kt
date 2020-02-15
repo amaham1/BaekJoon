402779edd4780e453a61124aeec06f4e1fc18b71
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-02-15 16:39:44 +0900

    Create 1008.kt

---
+++ src/1008.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main () {
+    val scanner = Scanner(System.`in`)
+    val a= scanner.nextDouble()
+    val b= scanner.nextDouble()
+    print(a/b)
+}
+//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
