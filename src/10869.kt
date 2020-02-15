0e2a310cba5339f49c9f0d7300ddd7bcf0dddba2
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-02-15 16:39:39 +0900

    Create 10869.kt

---
+++ src/10869.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main () {
+    val scanner = Scanner(System.`in`)
+
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    print(a%b)
+}
+//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
