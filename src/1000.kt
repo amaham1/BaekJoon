5a8613690787cbe63c404df5d3c1472cd0474914
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-02-15 16:39:34 +0900

    Create 1000.kt

---
+++ src/1000.kt
@@ -0,0 +1,7 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    print(scanner.nextInt() + scanner.nextInt())
+}
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
