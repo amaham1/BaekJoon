11f933e6f3c41bd8d29ec26647899f462d8bd6b9
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-02-18 15:57:46 +0900

    Update 10817.kt

--- src/10817.kt
+++ src/10817.kt
@@ -3,19 +3,20 @@ import java.util.*
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    val a = scanner.nextInt()
-    val b = scanner.nextInt()
-    val c = scanner.nextInt()
+    var a = scanner.nextInt()
+    var b = scanner.nextInt()
+    var c = scanner.nextInt()
 
-    if (a > b && b > c){
-        print(b)}
-        else if (a < b && b < c) {
-            print(b)
-        }
-        a < b && b > c && a > c-> print(a)
-        a < b && b > c && a < c-> print(c)
-
-        a > b && b < c && a > c-> print(c)
-        a > b && b < c && a < c-> print(a)
+    when {
+        a > b && a > c && b > c -> print(b)//a가 가장 크고 b가 2번째 일때
+        a > b && a > c && b < c -> print(c)//a가 가장 크고 c가 2번째 일때
+        b > a && b > c && a > c -> print(a)//b가 가장 크고 a가 2번째 일때
+        b > a && b > c && a < c -> print(c)//b가 가장 크고 c가 2번째 일때
+        c > a && c > b && a > b -> print(a)//c가 가장 크고 a가 2번째 일때
+        c > a && c > b && a < b -> print(b)//c가 가장 크고 b가 2번째 일때
+        a == b -> print(a)//숫자 2개가 같다는건 남은 1개는 작거나 크다는 얘기
+        b == c -> print(b)
+        a == c -> print(c)
     }
 }
+//세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오.
