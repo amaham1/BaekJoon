bd0c699f20b2c7bea7c49a73294452380e16075b
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-02-21 21:19:33 +0900

    Update 10818.kt

--- src/10818.kt
+++ src/10818.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     var str = br.readLine()//size 받는 구간
     val b = str.toInt()//size
-    var numbers = Array(b) {0} //사이즈가 0인 배열
+    var numbers = Array(b) {0} //사이즈가 b인 배열
 
     var stx = StringTokenizer(str)
     str = br.readLine() //배열의 원소 받을준비
@@ -17,9 +17,9 @@ fun main(args: Array<String>) {
     var min = Int.MAX_VALUE
     while (stx.hasMoreTokens()) {
         numbers[i] = stx.nextToken().toInt() //배열의 원소받기
-        if (numbers[i] < min) // 첫 번째 수 temp가 min ( 21억~ ) 보다 작으므로 temp가 min이 됨. temp는 첫 번째 수가 되고, 반복문이 돌아 다음 수와 비교
-            min = numbers[i] // temp와 비교하여 작은 수는 min 값이 됨
-        if (numbers[i] > max) // 첫 번째 수 temp가 max ( -21억~ ) 보다 크므로 temp가 min이 됨. temp는 첫 번째 수가 되고, 반복문이 돌아 다음 수와 비교
+        if (numbers[i] < min) // 첫 번째 수 numbers가 min ( 21억~ ) 보다 작으므로 numbers가 min이 됨. number는 첫 번째 수가 되고, 반복문이 돌아 다음 수와 비교
+            min = numbers[i] // number와 비교하여 작은 수는 min 값이 됨
+        if (numbers[i] > max) // 첫 번째 수 numbers가 max ( -21억~ ) 보다 크므로 numbers가 min이 됨. number는 첫 번째 수가 되고, 반복문이 돌아 다음 수와 비교
             max = numbers[i]
         i++
     }
