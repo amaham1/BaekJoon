753264f679c07c83764a947e19755ee8e84f27d1
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-03-04 18:10:25 +0900

    Create 2941.kt

---
+++ src/2941.kt
@@ -0,0 +1,25 @@
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    var alpha = br.readLine()
+    val wordArray = arrayOf("c=","c-","dz=","d-","lj","nj","s=","z=")
+
+    for ( i in wordArray.indices ) {
+        if(alpha.contains(wordArray[i])){
+            alpha = alpha.replace(wordArray[i],"1")
+        }
+    }
+    bw.write("${alpha.length}")
+    bw.flush()
+    bw.close()
+}
+//예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+//
+//dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
+//
+//입력
+//첫째 줄에 최대 100글자의 단어가 주어진다. 알파벳 소문자와 '-', '='로만 이루어져 있다.
+//
+//단어는 크로아티아 알파벳으로 이루어져 있다. 문제 설명의 표에 나와있는 알파벳은 변경된 형태로 입력된다.
