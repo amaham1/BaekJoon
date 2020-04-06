af7d71949b8005ba0464c84e44f01e395d44e381
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-04-06 13:58:21 +0900

    Create 2869.kt

---
+++ src/2869.kt
@@ -0,0 +1,25 @@
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val num = StringTokenizer(br.readLine())
+    val a = num.nextToken().toInt()
+    val b = num.nextToken().toInt()
+    val v = num.nextToken().toInt()
+
+    var abMinus = a - b
+    var sum =  (v - b - 1) / abMinus
+    bw.write("${sum + 1}")
+    bw.flush()
+    bw.close()
+}
+//땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+//
+//달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 또, 정상에 올라간 후에는 미끄러지지 않는다.
+//
+//달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B < A ≤ V ≤ 1,000,000,000)
